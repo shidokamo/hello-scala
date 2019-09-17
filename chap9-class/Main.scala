88ce5b69702ea0520bc4d28e3d37693256eb4f01
Kamome Shido <shidokamo@users.noreply.github.com>
2019-09-17 21:11:51 +0000

    エラー修正して、Main 関数に呼び出しを追加

--- chap9-class/Main.scala
+++ chap9-class/Main.scala
@@ -3,5 +3,8 @@ object Main {
     val p1 = new Point3D(1, 2, 3)
     val p2 = new Point3D(3, 2, 1)
     println(p1 + p2)
+    val adder = p1.add(2, _)
+    println(adder)
+    println(adder(4))
   }
 }
--- chap9-class/Point3D.scala
+++ chap9-class/Point3D.scala
@@ -2,8 +2,8 @@ class Point3D(val x: Int, val y: Int, val z: Int) {
   def +(p: Point3D): Point3D = {
     new Point3D(x + p.x, y + p.y, z + p.z)
   }
-  def add(a: Int, b: Int): Int = {
-    new Point3d(x+a+b, y+a+b, z+a+b)
+  def add(a: Int, b: Int): Point3D = {
+    new Point3D(x+a+b, y+a+b, z+a+b)
   }
   override def toString(): String = "(" + x + ", " + y + ", " + z + ")"
 }
