0a441788211d8c13557a7dfa846e4fdcabd9443e
Kamome Shido <shidokamo@users.noreply.github.com>
2019-09-17 21:03:41 +0000

    バグを修正

--- chap9-class/Point3D.scala
+++ chap9-class/Point3D.scala
@@ -1,6 +1,6 @@
 class Point3D(val x: Int, val y: Int, val z: Int) {
-  def +(p: Point): Point = {
-    new Point(x + p.x, y + p.y, z + p.z)
+  def +(p: Point3D): Point3D = {
+    new Point3D(x + p.x, y + p.y, z + p.z)
   }
   override def toString(): String = "(" + x + ", " + y + ", " + z + ")"
 }
