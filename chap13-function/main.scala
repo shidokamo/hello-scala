985a1eacc1f798f9f9fc1456ff381d57426f5e2a
Kamome Shido <shidokamo@users.noreply.github.com>
2019-09-21 03:27:32 +0000

    関数のコードを追加

--- chap13-function/main.scala
+++ chap13-function/main.scala
@@ -6,8 +6,14 @@ object Main {
     }
     println(add.apply(100, 200))
     println(add(100,200))
+
     // Syntax Suger
     val add2 = (x: Int, y: Int) => x + y
     println(add2(100,200))
+
+    // Direct call
+    println(
+      new Function2[Int, Int, Int]{ def apply(x: Int, y: Int): Int = x + y }.apply(100, 200)
+    )
   }
 }
