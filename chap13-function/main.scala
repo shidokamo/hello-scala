89fdec05544886edb61f6d9083012368fe700baf
Kamome Shido <shidokamo@users.noreply.github.com>
2019-09-21 03:16:00 +0000

    関数の例を追加

---
+++ chap13-function/main.scala
@@ -0,0 +1,13 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    // Lambda Instance
+    val add = new Function2[Int, Int, Int]{
+      def apply(x: Int, y: Int): Int = x + y
+    }
+    println(add.apply(100, 200))
+    println(add(100,200))
+    // Syntax Suger
+    val add2 = (x: Int, y: Int) => x + y
+    println(add2(100,200))
+  }
+}
