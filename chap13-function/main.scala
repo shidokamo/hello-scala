6be6ca8a747b1d2c3ccd0df99e59951c7188244e
Kamome Shido <shidokamo@users.noreply.github.com>
2019-09-21 03:34:56 +0000

    関数のコードを追加

--- chap13-function/main.scala
+++ chap13-function/main.scala
@@ -11,12 +11,17 @@ object Main {
     val add2 = (x: Int, y: Int) => x + y
     println(add2(100,200))
 
-    // Direct call
+    // Direct call (apply を別の名前に変更はできない。)
     println(
       new Function2[Int, Int, Int]{ def apply(x: Int, y: Int): Int = x + y }.apply(100, 200)
     )
+    // apply を省略してもコンパイラは解釈してくれる。（syntax suger)
     println(
       (new Function2[Int, Int, Int]{ def apply(x: Int, y: Int): Int = x + y })(100, 200)
+    )
+    // Syntax Suger を使って、簡略化できる。
+    println(
+      ((x: Int, y: Int) => x + y)(100, 200)
     )
   }
 }
